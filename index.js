2ec84152465f4564db47d3458f25706c785415e1
mostofa kamal <mostofakamal1990@gmail.com>
2022-07-19 23:47:55 +0600

    react core concept three

---
+++ index.js
@@ -0,0 +1,40 @@
+const redux = require('redux');
+
+const createStore = redux.createStore
+
+const Buy_CAKE = 'BUY_CAKE';
+
+function buyCake(){
+    return {
+        type:Buy_CAKE,
+        info: 'First redux action'
+    }
+}
+
+
+// (PreviousState, action ) => newState
+
+
+const initialState={
+    numOfCakes: 10
+}
+
+const reducer = (state = initialState, action)=>{
+    switch(action.type){
+        case Buy_CAKE: return{
+            ...state,
+            numOfCakes: state.numOfCakes - 1
+        }
+
+        default: return state
+    }
+}
+
+const store = createStore(reducer );
+
+console.log('initial State', store.getStore());
+
+store.subscribe(()=> console.log("update state", store.getState()));
+store.dispatch(buyCake())
+store.dispatch(buyCake())
+store.dispatch(buyCake())
